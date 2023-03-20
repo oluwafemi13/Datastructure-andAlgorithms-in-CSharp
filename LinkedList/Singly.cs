8f2b08dd65172a8998281af30d61440f4f6e2f0d
IKECHUKWU <sundayjabikem@gmail.com>
2023-03-20 09:59:06 +0100

    creating a linkedList

--- LinkedList/Singly.cs
+++ LinkedList/Singly.cs
@@ -8,5 +8,22 @@ namespace LinkedList
 {
     public class Singly
     {
+        public Node head;
+        public Node tail;
+        public int size;
+
+
+        public Node CreateSinglyLinkedList(int value)
+        {
+            head = new Node();
+            Node node = new Node();
+            node.value = value;
+            node.Next = null;
+            head= node;
+            tail= node;
+            size = 1;
+            return node;
+
+        }
     }
 }
